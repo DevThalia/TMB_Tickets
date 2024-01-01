4cddb1b3d3313363d2f4f60819772be92083a53e
Anna <anna.elisabet5@gmail.com>
2024-01-01 17:02:05 +0100

    created fun messageErrorDT() and messageErrorRange()

--- src/main/kotlin/Messages.kt
+++ src/main/kotlin/Messages.kt
@@ -26,3 +26,13 @@ fun endOfProgramMessage(){
     println("    Reculli el seu tiquet.  ")
     println("        ¡Bon Viatge!        ")
 }
+
+fun messageErrorDT(){
+    //funcion que es llamada cuando el tipo de dato es incorrecto
+    println(RED_BOLD+"El tipus de dada es incorrecte, torni a intentar."+RESET)
+}
+
+fun messageErrorRange(){
+    //funcion que es llamada cuando el tipo de dato esta fuera del rango establecido
+    println(YELLOW_BOLD+"El tipus de dada no està en les opcions, torni a intentar."+RESET)
+}
